dbc2ae672ad3a461affd40a47553a45155f1c0d9
zynov <zynov1997@mail.ru>
2020-12-20 16:59:36 +0300

    added <tbody>

--- ishop/MyComponent.js
+++ ishop/MyComponent.js
@@ -23,13 +23,15 @@ var ProductListTable = React.createClass({
         return React.DOM.div( {className:'ProductListTable'}, 
                 React.DOM.div( {className:'Title'}, this.props.title ),
                 React.DOM.table( {className:'List'}, 
-                    React.DOM.tr( {className: 'Product-table__row'},
-                        React.DOM.td({className: 'Product-table__img'}, 'image'),
-                        React.DOM.td({className: 'Product-table__name'}, 'name'),
-                        React.DOM.td({className: 'Product-table__quantity'}, 'quantity'),
-                        React.DOM.td({className: 'Product-table__cost'}, 'cost'),
-                    ),
-                    productListArr 
+                    React.DOM.tbody(null,
+                        React.DOM.tr( {className: 'Product-table__row'},
+                            React.DOM.td({className: 'Product-table__img'}, 'image'),
+                            React.DOM.td({className: 'Product-table__name'}, 'name'),
+                            React.DOM.td({className: 'Product-table__quantity'}, 'quantity'),
+                            React.DOM.td({className: 'Product-table__cost'}, 'cost'),
+                        ),
+                        productListArr
+                    )
                 ),
             );
     },
